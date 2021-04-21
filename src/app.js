bd62d3f93e4bde868d954af0d837c1106fb5ad2c
Andreagar28 <agarchig28@gmail.com>
2021-04-21 14:09:46 +0000

    day 6 domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net", ".us", ".io"];
+  let all = "";
+  let allOptions = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          all = all.concat(pronoun[i], adj[j], noun[k], dom[l]);
+          //console.log(pronoun[i] + adj[j] + noun[k] + dom[l]);
+          allOptions.push(all);
+          all = "";
+        }
+      }
+    }
+  }
+  console.log(allOptions);
+  create(allOptions);
 };
+
+function create(allOptions) {
+  for (let g = 0; g < allOptions.length; g++) {
+    let newItem = document.querySelector("#list");
+    let newOne = document.createElement("li");
+    let textNew = document.createTextNode(allOptions[g]);
+    newOne.appendChild(textNew);
+    newItem.appendChild(newOne);
+  }
+}
